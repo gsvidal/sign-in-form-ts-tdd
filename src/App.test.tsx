b026753caad8fbfef748c0c063bf55fc0c7f83a9
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-01-31 19:20:38 -0500

    Add sass setup and use TDD to create three inputs within a signin form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('inputs should be initially empty', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInputElement: HTMLInputElement = screen.getByRole('textbox');
+  const passwordInputElement: HTMLInputElement =
+    screen.getByLabelText(/^password$/i);
+  const confirmPasswordInputElement: HTMLInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  expect(emailInputElement.value).toBe('');
+  expect(passwordInputElement.value).toBe('');
+  expect(confirmPasswordInputElement.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement: HTMLInputElement = screen.getByRole('textbox', {
+    name: /email address/i,
+  });
+  const inputEmail = 'wiu@gmail.com';
+  userEvent.type(emailInputElement, inputEmail);
+  expect(emailInputElement.value).toBe(inputEmail);
+});
+
+test('should be able to type a password', () => {
+  render(<App />);
+  const passwordInputElement: HTMLInputElement =
+    screen.getByLabelText(/^password$/i);
+  const inputPassword = 'wiu666';
+  userEvent.type(passwordInputElement, inputPassword);
+  expect(passwordInputElement.value).toBe(inputPassword);
+});
+
+test('should be able to type a password', () => {
+  render(<App />);
+  const confirmPasswordInputElement: HTMLInputElement =
+    screen.getByLabelText(/confirm password/i);
+  const inputPassword = 'wiu666';
+  userEvent.type(confirmPasswordInputElement, inputPassword);
+  expect(confirmPasswordInputElement.value).toBe(inputPassword);
+});
+
+test('should show email error message on invalid email', () => {
+  render(<App />);
+  const emailInputElement: HTMLInputElement = screen.getByRole('textbox', {
+    name: /email address/i,
+  });
+  const submitButtonElement: HTMLButtonElement = screen.getByRole('button', {
+    name: /submit/i,
+  });
+  type emailErrorMessageType = HTMLParagraphElement | null;
+  const emailErrorMessage: emailErrorMessageType = screen.queryByText(
+    /the email you input is invalid/i
+  );
+
+  expect(emailErrorMessage).not.toBeInTheDocument();
+
+  //regex for gmail
+  userEvent.type(emailInputElement, 'wiugmail.com');
+  userEvent.click(submitButtonElement);
+  const emailErrorMessage2: HTMLParagraphElement = screen.getByText(
+    /the email you input is invalid/i
+  );
+  expect(emailErrorMessage2).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import './App.scss';
+import validator from 'validator';
+
+type InputType = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type showErrorMessageType = {
+  email: boolean;
+  password: boolean;
+  confirmPassword: boolean;
+};
 
 function App() {
+  const [input, setInput] = useState<InputType>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [showErrorMessage, setShowErrorMessage] =
+    useState<showErrorMessageType>({
+      email: false,
+      password: false,
+      confirmPassword: false,
+    });
+
+  const handleSubmit: (event: FormEvent<HTMLFormElement>) => void = (event) => {
+    event.preventDefault();
+    setShowErrorMessage({
+      ...showErrorMessage,
+      email: !validator.isEmail(input.email),
+    });
+  };
+
+  const handleChange: (event: ChangeEvent<HTMLInputElement>) => void = (
+    event
+  ) => {
+    setInput({
+      ...input,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form action="" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-control"
+          value={input.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <div>
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input type="password" id="confirm-password" name="confirm-password" />
+      </div>
+      {showErrorMessage.email && <p>The email you input is invalid</p>}
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
